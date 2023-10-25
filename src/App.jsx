43e79f47a4158afb30eaf5e60c04a542e67af8fc
Abhishek Ajmera <abhishekajmera17@gmail.com>
2023-10-25 21:39:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+// import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+        <Header />
+      {/* <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<App />}
+          />
+        </Routes>
+      </BrowserRouter> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { Container, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Navbar
+      bg='dark'
+      variant='dark'
+      style={{ height: 80 }}
+    >
+      <Container>
+        <Navbar.Brand>
+          <a href='/'>Something</a>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
